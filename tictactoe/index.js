b553edb141be8959a1d7a0280706492ed264fc16
Anil16Kumar <87219848+Anil16Kumar@users.noreply.github.com>
2023-06-29 22:10:31 +0530

    Add files via upload

---
+++ tictactoe/index.js
@@ -0,0 +1,43 @@
+const Game = require("./game");
+let symbol1 = "X";
+let symbol2 = "O";
+try { 
+    let g1 = Game.addGame("Anil", symbol1, "Atul", symbol2);
+    console.log(g1.play(1));
+    console.log(g1.play(8));
+    console.log(g1.play(7));
+    console.log(g1.play(4));
+    console.log(g1.play(0));
+    console.log(g1.play(3));
+    console.log(g1.play(2)); 
+ 
+    let g2 = Game.addGame("raju", symbol2, "shyam", symbol1);
+    console.log(g2.play(9));
+    console.log(g2.play(0));
+    console.log(g2.play(1));
+    console.log(g2.play(2));
+    console.log(g2.play(3));
+    console.log(g2.play(4));
+    console.log(g2.play(5));
+    console.log(g2.play(6));
+    console.log(g2.play(7));  
+    console.log(g2.play(8));
+    console.log(g2.play(9));
+
+
+    let g3 = Game.addGame("Abhay", symbol2, "Anil", symbol1);
+    console.log(g3.play(9));
+    console.log(g3.play(0));
+    console.log(g3.play(1));
+    console.log(g3.play(2));
+    console.log(g3.play(3));
+    console.log(g3.play(4));
+    console.log(g3.play(5));
+    console.log(g3.play(6));
+    console.log(g3.play(7));  
+    console.log(g3.play(8));
+    console.log(g3.play(9));
+
+} catch (error) {
+    console.log(error.message);
+}
